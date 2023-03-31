0b0949edc96e9d0b65759906ad6540005b9b351e
Dan Moisan <drmoisan@gmail.com>
2023-03-31 12:29:09 -0700

    Built IQfcItemController Interface

---
+++ QuickFiler/Interfaces/IQfcItemController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Office.Interop.Outlook;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickFiler
+{
+    internal interface IQfcItemController
+    {
+        /// <summary>
+        /// Toggles special formatting for one group to highlight the group of controls that is active
+        /// </summary>
+        void Accel_FocusToggle(); // Turn on or off the formatting to highlight this QfcItem
+        
+        
+        void Accel_Toggle();
+        void ctrlsRemove(); // May not be necessary
+        bool BlExpanded {  get; }
+        bool BlHasChild { get; set; }
+        int Height { get; }
+        MailItem Mail { get; set; }
+        void ExpandCtrls1(); // Rewrite
+        void PopulateFolderCombobox(object varList = null); // Handles just the UI aspect. Relies on FolderSuggestionsModule.Folder_Suggestions
+        void ApplyReadEmailFormat();
+        void FlagAsTask();
+        void MarkItemForDeletion();
+        void JumpToSearchTextbox();
+        void JumpToFolderDropDown();
+        void ToggleDeleteFlow();
+        void ToggleSaveCopyOfMail();
+        void ToggleSaveAttachments();
+        void ToggleConversationCheckbox();
+    }
+}
