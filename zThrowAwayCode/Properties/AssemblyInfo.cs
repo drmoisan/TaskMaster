f1484f9fe6b8333971265b66f77561aa4f780e68
drmoisan <54180981+drmoisan@users.noreply.github.com>
2023-07-28 15:04:14 -0700

    Refactor/test utilities (#15)
    * Organized UtilitiesCS tests into folders.
    * Added new temp project for code used in development

---
+++ zThrowAwayCode/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("zThrowAwayCode")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("PepsiCo Inc")]
+[assembly: AssemblyProduct("zThrowAwayCode")]
+[assembly: AssemblyCopyright("Copyright © PepsiCo Inc 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("96bed86b-ff33-44b1-8a28-914c194ffbc1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
