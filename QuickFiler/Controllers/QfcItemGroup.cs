d6e06c7e7a5f06fc724cdea96b862b066c55f332
Dan Moisan <drmoisan@gmail.com>
2023-09-28 21:52:31 -0700

    WIP

---
+++ QuickFiler/Controllers/QfcItemGroup.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Office.Interop.Outlook;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QuickFiler.Interfaces;
+using static QuickFiler.Controllers.QfcCollectionController;
+using System.Windows.Forms;
+using UtilitiesCS;
+using System.Collections.Specialized;
+using System.ComponentModel;
+
+namespace QuickFiler.Controllers
+{
+    public class QfcItemGroup
+    {
+        public QfcItemGroup() { }
+        public QfcItemGroup(MailItem mailItem) { _mailItem = mailItem; }
+
+        private IQfcItemController _itemController;
+        private MailItem _mailItem;
+
+        private ItemViewer _itemViewer;
+        internal ItemViewer ItemViewer { get => _itemViewer; set => _itemViewer = value; }
+
+        internal IQfcItemController ItemController { get => _itemController; set => _itemController = value; }
+        internal MailItem MailItem { get => _mailItem; set => _mailItem = value; }
+    }
+
+}
---
+++ QuickFiler/Controllers/QfcQueue.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.Office.Interop.Outlook;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using UtilitiesCS;
+
+namespace QuickFiler.Controllers
+{
+    public class QfcQueue : INotifyCollectionChanged, INotifyPropertyChanged
+    {
+        public QfcQueue() { }
+        
+        private TableLayoutPanel _tlpTemplate;
+        public TableLayoutPanel TlpTemplate 
+        { 
+            get => _tlpTemplate;
+            set
+            {
+                _tlpTemplate = value.Clone();
+                _tlpTemplate.Name = "TemplateTableLayout";
+            }
+        }
+
+        private Queue<(TableLayoutPanel Tlp, List<QfcItemGroup> ItemGroups)> _queue = new();
+        //public Queue<(TableLayoutPanel Tlp, List<QfcItemGroup> ItemGroups)> Queue { get => _queue; }
+
+        #region Queue Functions
+
+        public (TableLayoutPanel Tlp, List<QfcItemGroup> ItemGroups) Dequeue()
+        {
+            (TableLayoutPanel tlp, List<QfcItemGroup> itemGroups) = _queue.Dequeue();
+            CollectionChanged?.Invoke(this, new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Remove, _queue));
+            return (tlp, itemGroups);
+        }
+
+        public async Task EnqueueAsync(IList<MailItem> items)
+        {
+            if (items is null) { throw new ArgumentNullException(nameof(items)); }
+            if (items.Count == 0) { throw new ArgumentException("items is empty"); }
+
+            var tlp = await UiIdleCallAsync(() => _tlpTemplate.Clone(name: "BackgroundTableLayout"));
+            var itemTasks = Enumerable.Range(0, items.Count)
+                                      .ToAsyncEnumerable()
+                                      .SelectAwait(async i => await AddAsync(tlp, items[i], i))
+                                      .ToListAsync();
+            var itemGroups = await itemTasks;
+            _queue.Enqueue((tlp, itemGroups));
+            CollectionChanged?.Invoke(this, new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, _queue));
+        }
+
+        internal async Task<QfcItemGroup> AddAsync(TableLayoutPanel tlp, MailItem mailItem, int indexNumber)
+        {
+            var grp = new QfcItemGroup(mailItem);
+            var viewer = ItemViewerQueue.Dequeue();
+            grp.ItemViewer = viewer;
+            await UiIdleCallAsync(() => AddViewerToTlp(tlp, viewer, indexNumber));
+            return grp;
+        }
+
+        internal void AddViewerToTlp(TableLayoutPanel tlp, ItemViewer viewer, int indexNumber)
+        {
+            viewer.Parent = tlp;
+            tlp.SetCellPosition(viewer, new TableLayoutPanelCellPosition(0, indexNumber));
+            tlp.SetColumnSpan(viewer, 2);
+            viewer.AutoSize = true;
+            viewer.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            viewer.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            viewer.Dock = DockStyle.Fill;
+        }
+
+        public int Count => _queue.Count;
+
+        #endregion
+
+        #region INotify
+
+        protected void NotifyPropertyChanged([System.Runtime.CompilerServices.CallerMemberName] string propertyName = "")
+        {
+            if (PropertyChanged is not null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public event NotifyCollectionChangedEventHandler CollectionChanged;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+
+        internal async Task UiIdleCallAsync(System.Action action)
+        {
+            await UIThreadExtensions.UiDispatcher.InvokeAsync(action, System.Windows.Threading.DispatcherPriority.ContextIdle);
+        }
+
+        internal async Task<T> UiIdleCallAsync<T>(Func<T> func)
+        {
+            return await UIThreadExtensions.UiDispatcher.InvokeAsync(func, System.Windows.Threading.DispatcherPriority.ContextIdle);
+        }
+    }
+
+}
+
