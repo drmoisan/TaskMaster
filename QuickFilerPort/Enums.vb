6ec191c660d0fd0a53d03dae897db38c650eb22a
Dan Moisan <drmoisan@gmail.com>
2023-10-23 09:15:01 -0700

    Prune unused working files

--- QuickFilerPort/Enums.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Module Enums
-    Public Enum InitTypeEnum
-        InitSort = 1                    '00000000 00000001   2^0
-        InitFind = 2                    '00000000 00000010   2^1
-        InitInfo = 4                    '00000000 00000100   2^2
-        InitConditionalReminder = 8     '00000000 00001000   2^3
-    End Enum
-
-End Module
