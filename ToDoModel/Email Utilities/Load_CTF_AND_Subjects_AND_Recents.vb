b51dc6c4bb090e07712f9b3592c8a085c51623f5
Dan Moisan <drmoisan@gmail.com>
2023-11-01 08:54:39 -0700

    Remove old VB Files

--- ToDoModel/Email Utilities/Load_CTF_AND_Subjects_AND_Recents.vb
+++
@@ -1,5 +0,0 @@
-﻿Public Module LoadCTFANDSubjectsANDRecents
-    Public Sub Load_CTF_AND_Subjects_AND_Recents()
-        Throw New NotImplementedException("Need to investigate if this is duplicative with existing functionality")
-    End Sub
-End Module
