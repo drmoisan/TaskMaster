b7107005d1a2773b24d0008bc072b5d9a6473235
Dan Moisan <drmoisan@gmail.com>
2023-12-11 13:43:26 -0800

    Bugfix RecentsList -> Race effect during concurrency led to duplicate entries and more than max number. Created blocking producer / consumer

--- UtilitiesCS/EmailIntelligence/Recents/RecentsList.cs
+++ UtilitiesCS/EmailIntelligence/Recents/RecentsList.cs
@@ -1,22 +1,51 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace UtilitiesCS
 {
     public class RecentsList<T> : ScoCollection<T>
     {
-        public RecentsList() : base() { }
-        public RecentsList(List<T> listOfT, int max) : base(listOfT) { Max = max; }
-        public RecentsList(IEnumerable<T> IEnumerableOfT, int max) : base(IEnumerableOfT) { Max = max; }
-        public RecentsList(string filename, string folderpath, int max) : base(filename, folderpath) { Max = max;}
+        public RecentsList() : base() 
+        {
+            _ct = _cts.Token;
+            _addTask = Task.Run(() => NonBlockingConsumer_Add(_ct), _ct);
+        }
+        
+        public RecentsList(List<T> listOfT, int max) : base(listOfT) 
+        { 
+            Max = max;
+            _ct = _cts.Token;
+            _addTask = Task.Run(() => NonBlockingConsumer_Add(_ct), _ct);
+        }
+        
+        public RecentsList(IEnumerable<T> IEnumerableOfT, int max) : base(IEnumerableOfT) 
+        { 
+            Max = max;
+            _ct = _cts.Token;
+            _addTask = Task.Run(() => NonBlockingConsumer_Add(_ct), _ct);
+        }
+        
+        public RecentsList(string filename, string folderpath, int max) : base(filename, folderpath) 
+        { 
+            Max = max;
+            _ct = _cts.Token;
+            _addTask = Task.Run(() => NonBlockingConsumer_Add(_ct), _ct);
+        }
 
         private int _max = 5;
         public int Max { get => _max; set => _max = value; }
         
         public new void Add(T item)
+        {
+            _bc.TryAdd(item);
+        }
+
+        private void AddThreadsafe(T item)
         {
             if (base.Contains(item))
             {
@@ -24,7 +53,7 @@ namespace UtilitiesCS
             }
             else
             {
-                if (base.Count > 0)
+                if (base.Count >= Max)
                 {
                     var idx = Math.Min(base.Count, Max) - 1;
                     base.RemoveAt(idx);
@@ -32,6 +61,31 @@ namespace UtilitiesCS
             }
             base.Insert(0, item);
             base.Serialize();
+        }
+
+        private BlockingCollection<T> _bc = [];
+        private readonly CancellationTokenSource _cts = new CancellationTokenSource();
+        private CancellationToken _ct;
+        private Task _addTask; 
+        private void NonBlockingConsumer_Add(CancellationToken ct) 
+        {
+            while (!_bc.IsCompleted)
+            {
+                try
+                {
+                    if (_bc.TryTake(out T nextItem, 0, ct))
+                    {
+                        AddThreadsafe(nextItem);
+                    }
+                }
+
+                catch (OperationCanceledException)
+                {
+                    Console.WriteLine("Taking canceled.");
+                    break;
+                }
+                
+            }
         }
     }
 }
