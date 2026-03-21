518aea1e8636ffe3177d48765b396730e6ecf6b8
Dan Moisan <drmoisan@gmail.com>
2026-03-21 11:27:45 -0400

    (test(utilities-coverage)): deepen WrapperPeopleScoDictionaryNew coverage
    - add MSTest coverage for null-guard, config restoration, copy, and derived-conversion paths
    - verify emitted Config and backing-field behavior with dynamic type builders across getter-only and missing-getter scenarios
    - update VS Code workspace recommendations and disable the Copilot CLI session controller in repo settings
    
    Refs: #87

--- UtilitiesCS.Test/NewtonsoftHelpers/WrapperPeopleScoDictionaryNew_Tests.cs
+++ UtilitiesCS.Test/NewtonsoftHelpers/WrapperPeopleScoDictionaryNew_Tests.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
 using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
@@ -130,6 +133,262 @@ namespace UtilitiesCS.Test.NewtonsoftHelpers
             // Assert
             result.CoDictionary.Should().NotBeNull();
             result.CoDictionary.Should().BeEmpty();
+        }
+
+        [TestMethod]
+        public void ToComposition_WithNullDerivedInstance_ThrowsArgumentNullException()
+        {
+            // Arrange
+            var wrapper = new WrapperPeopleScoDictionaryNew();
+
+            // Act
+            Action act = () => wrapper.ToComposition(null);
+
+            // Assert
+            act.Should().Throw<ArgumentNullException>();
+        }
+
+        [TestMethod]
+        public void ToDerived_WhenCoDictionaryIsNull_ThrowsArgumentNullException()
+        {
+            // Arrange
+            var wrapper = new WrapperPeopleScoDictionaryNew
+            {
+                CoDictionary = null,
+                RemainingObject = new PeopleScoRemainingObject(),
+            };
+
+            // Act
+            Action act = () => wrapper.ToDerived();
+
+            // Assert
+            act.Should().Throw<ArgumentNullException>();
+        }
+
+        [TestMethod]
+        public void ToDerived_WhenRemainingObjectIsNull_ThrowsArgumentNullException()
+        {
+            // Arrange
+            var wrapper = new WrapperPeopleScoDictionaryNew { RemainingObject = null };
+
+            // Act
+            Action act = () => wrapper.ToDerived();
+
+            // Assert
+            act.Should().Throw<ArgumentNullException>();
+        }
+
+        [TestMethod]
+        public void ToDerived_WhenConfigFieldIsMissing_StillRestoresDictionaryEntries()
+        {
+            // Arrange
+            var wrapper = new WrapperPeopleScoDictionaryNew();
+            wrapper.CoDictionary.TryAdd("key", "value");
+            wrapper.RemainingObject = new PeopleScoRemainingObject();
+
+            // Act
+            var restored = wrapper.ToDerived();
+
+            // Assert
+            restored.Should().ContainKey("key");
+            restored["key"].Should().Be("value");
+        }
+
+        [TestMethod]
+        public void CompileType_CreatesTypeWithConfigProperty()
+        {
+            // Arrange
+            var wrapper = new WrapperPeopleScoDictionaryNew();
+
+            // Act
+            var createdType = wrapper.CompileType();
+
+            // Assert
+            createdType.Should().NotBeNull();
+            var configProperty = createdType.GetProperty(
+                "Config",
+                BindingFlags.Instance | BindingFlags.Public
+            );
+            configProperty.Should().NotBeNull();
+            configProperty.CanRead.Should().BeTrue();
+            configProperty.CanWrite.Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void CopyTo_WithRemainingObjectType_CopiesConfig()
+        {
+            // Arrange
+            var wrapper = new WrapperPeopleScoDictionaryNew();
+            var derived = new PeopleScoDictionaryNew { Config = new NewSmartSerializableConfig() };
+
+            // Act
+            var copied = wrapper.CopyTo(derived, typeof(PeopleScoRemainingObject));
+
+            // Assert
+            copied.Should().BeOfType<PeopleScoRemainingObject>();
+            ((PeopleScoRemainingObject)copied).Config.Should().NotBeNull();
+        }
+
+        [TestMethod]
+        public void CreateConfigProperty_CreatesReadableConfigPropertyAndBackingField()
+        {
+            // Arrange
+            var wrapper = new WrapperPeopleScoDictionaryNew();
+            using var typeBuilderScope = new TypeBuilderScope("WrapperPeopleScoCreateConfig");
+            var expectedConfig = new NewSmartSerializableConfig();
+
+            // Act
+            wrapper.CreateConfigProperty(typeBuilderScope.TypeBuilder);
+            var generatedType = typeBuilderScope.TypeBuilder.CreateType();
+            var generatedInstance = Activator.CreateInstance(generatedType);
+            generatedType
+                .GetField("_Config", BindingFlags.Instance | BindingFlags.NonPublic)
+                .SetValue(generatedInstance, expectedConfig);
+
+            // Assert
+            generatedType
+                .GetProperty("Config")
+                .GetValue(generatedInstance)
+                .Should()
+                .BeSameAs(expectedConfig);
+        }
+
+        [TestMethod]
+        public void ReplicateProperty_WithExplicitBackingField_CreatesRoundTrippableProperty()
+        {
+            // Arrange
+            var wrapper = new WrapperPeopleScoDictionaryNew();
+            var property = typeof(PeopleScoRemainingObject).GetProperty(
+                nameof(PeopleScoRemainingObject.Name)
+            );
+            var backingField = typeof(PeopleScoRemainingObject).GetField(
+                "<Name>k__BackingField",
+                BindingFlags.Instance | BindingFlags.NonPublic
+            );
+            using var typeBuilderScope = new TypeBuilderScope("WrapperPeopleScoExplicitField");
+
+            // Act
+            wrapper.ReplicateProperty(typeBuilderScope.TypeBuilder, property, backingField);
+            var replicatedType = typeBuilderScope.TypeBuilder.CreateType();
+            var replicatedInstance = Activator.CreateInstance(replicatedType);
+            replicatedType.GetProperty(property.Name).SetValue(replicatedInstance, "replicated");
+
+            // Assert
+            replicatedType
+                .GetProperty(property.Name)
+                .GetValue(replicatedInstance)
+                .Should()
+                .Be("replicated");
+            replicatedType
+                .GetField("<Name>k__BackingField", BindingFlags.Instance | BindingFlags.NonPublic)
+                .GetValue(replicatedInstance)
+                .Should()
+                .Be("replicated");
+        }
+
+        [TestMethod]
+        public void ReplicateProperty_WhenSetterIsMissing_SkipsSetterAndCreatesReadableProperty()
+        {
+            // Arrange
+            var wrapper = new WrapperPeopleScoDictionaryNew();
+            var property = typeof(GetterOnlyHolder).GetProperty(nameof(GetterOnlyHolder.Value));
+            var capturedFields = new Dictionary<string, FieldBuilder>();
+            using var typeBuilderScope = new TypeBuilderScope("WrapperPeopleScoGetterOnly");
+
+            // Act
+            wrapper.ReplicateProperty(typeBuilderScope.TypeBuilder, property, ref capturedFields);
+            var replicatedType = typeBuilderScope.TypeBuilder.CreateType();
+
+            // Assert
+            replicatedType.GetProperty(property.Name).CanRead.Should().BeTrue();
+            replicatedType.GetProperty(property.Name).CanWrite.Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void ReplicateProperty_WhenGetterIsMissing_ThrowsInvalidOperationException()
+        {
+            // Arrange
+            var wrapper = new WrapperPeopleScoDictionaryNew();
+            var property = typeof(SetterOnlyHolder).GetProperty(nameof(SetterOnlyHolder.Value));
+            var capturedFields = new Dictionary<string, FieldBuilder>();
+            using var typeBuilderScope = new TypeBuilderScope("WrapperPeopleScoSetterOnly");
+
+            // Act
+            Action act = () =>
+                wrapper.ReplicateProperty(
+                    typeBuilderScope.TypeBuilder,
+                    property,
+                    ref capturedFields
+                );
+
+            // Assert
+            act.Should().Throw<InvalidOperationException>().WithMessage("*getter*");
+        }
+
+        [TestMethod]
+        public void GetBackingField_WhenPropertyHasBackingField_ReturnsUnderlyingField()
+        {
+            // Arrange
+            var wrapper = new WrapperPeopleScoDictionaryNew();
+            var property = typeof(PeopleScoRemainingObject).GetProperty(
+                nameof(PeopleScoRemainingObject.Name)
+            );
+
+            // Act
+            var backingField = wrapper.GetBackingField(property);
+
+            // Assert
+            backingField.Name.Should().Be("<Name>k__BackingField");
+        }
+
+        [TestMethod]
+        public void GetBackingField_WhenGetterIsMissing_ThrowsInvalidOperationException()
+        {
+            // Arrange
+            var wrapper = new WrapperPeopleScoDictionaryNew();
+            var property = typeof(SetterOnlyHolder).GetProperty(nameof(SetterOnlyHolder.Value));
+
+            // Act
+            Action act = () => wrapper.GetBackingField(property);
+
+            // Assert
+            act.Should().Throw<InvalidOperationException>().WithMessage("*getter*");
+        }
+
+        private sealed class GetterOnlyHolder
+        {
+            public string Value => "getter";
+        }
+
+        private sealed class SetterOnlyHolder
+        {
+            public string Value
+            {
+                set => Stored = value;
+            }
+
+            public string Stored { get; private set; }
+        }
+
+        private sealed class TypeBuilderScope : IDisposable
+        {
+            public TypeBuilderScope(string typeName)
+            {
+                var assemblyName = new AssemblyName($"{typeName}_{Guid.NewGuid():N}");
+                var assemblyBuilder = AppDomain.CurrentDomain.DefineDynamicAssembly(
+                    assemblyName,
+                    AssemblyBuilderAccess.Run
+                );
+                var moduleBuilder = assemblyBuilder.DefineDynamicModule("MainModule");
+                TypeBuilder = moduleBuilder.DefineType(
+                    typeName,
+                    TypeAttributes.Public | TypeAttributes.Class
+                );
+            }
+
+            public TypeBuilder TypeBuilder { get; }
+
+            public void Dispose() { }
         }
     }
 }
